818d2944ffd3e02f159170becf163d59553de662
d-shlenskii <daniil.shlenskii@gmail.com>
2025-05-31 17:05:12 +0300

    set_environment script udpated

--- set_environment.sh
+++ set_environment.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export PROJECT_PATH=$SCRIPT_DIR
-export SRC_DIR=${PROJECT_PATH}/src
+export PROJECT_DIR=$SCRIPT_DIR
+export SRC_DIR=${PROJECT_DIR}/src
 
 export PYTHONPATH=${SRC_DIR}:${PYTHONPATH}
