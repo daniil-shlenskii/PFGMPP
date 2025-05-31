f6e859d583f85ea1d66e207ded809c8108899cbf
d-shlenskii <daniil.shlenskii@gmail.com>
2025-05-31 17:32:31 +0300

    pfgmpp guided ameliorated

--- src/pfgmpp/pfgmpp_guided.py
+++ src/pfgmpp/pfgmpp_guided.py
@@ -17,11 +17,13 @@ class PFGMPPGuided(PFGMPP):
         self,
         cls: nn.Module,
         cls_optimizer: torch.optim.Optimizer,
+        cls_sigma_prior_mode: str = "sqrt",
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.cls = cls
         self.cls_optimizer = cls_optimizer
+        self.cls_sample_from_sigma_prior = self._get_sigma_prior(cls_sigma_prior_mode)
 
     @torch.no_grad()
     def sample(self, *, label: LongTensor, guidance_scale: float, **kwargs):
@@ -31,10 +33,10 @@ class PFGMPPGuided(PFGMPP):
     @torch.no_grad()
     def sample_unconditional(self, **kwargs):
         self.cls.eval()
-        return self.sample(label=None, guidance_scale=0., **kwargs)
+        return self.sample(guidance_scale=0., **kwargs)
 
     def drift(self, x_hat: Tensor, t: Tensor, label: LongTensor, guidance_scale: float=0., **kwargs):
-        uncoditional_drift = self.model.drift(x_hat=x_hat, t=t, D=self.D, label=None)
+        uncoditional_drift = self.model.drift(x_hat=x_hat, t=t, D=self.D, label=label)
         classifer_score = (
             self.classifier_score(x_hat=x_hat, t=t, label=label)
             if label is not None
@@ -47,32 +49,44 @@ class PFGMPPGuided(PFGMPP):
             x_hat.requires_grad_(True)
             logits = self.cls(x=x_hat, t=t)
             log_probs = F.log_softmax(logits, dim=-1)
-            class_log_prob = log_probs[:, label].sum()
-            score = torch.autograd.grad(class_log_prob, x_hat)[0]
-            return score
+            class_log_probs = log_probs[torch.arange(len(logits)), label.view(-1)]
+            # print(log_probs.exp()[0].detach().numpy(), class_log_probs[0].exp().item(), label[0].item())
+            return torch.autograd.grad(class_log_probs.sum(), x_hat)[0]
 
     def train_classifier(self, train_loader: torch.utils.data.DataLoader, n_iters: int, verbose: bool=True, log_every: int=100):
         train_loader = get_inifinite_loader(train_loader)
         pbar = tqdm(train_loader, total=n_iters, dynamic_ncols=True, colour="green", disable=not verbose)
         acc_batch_loss, acc_batch_score = 0., 0.
+        acc_sigma_min_loss, acc_sigma_mean_loss, acc_sigma_max_loss = 0., 0., 0.
         acc_sigma_min_score, acc_sigma_mean_score, acc_sigma_max_score = 0., 0., 0.
         for i, (x, label) in enumerate(pbar):
             batch_loss, batch_score = self._train_classifier_step(x=x, label=label)
             acc_batch_loss += batch_loss / log_every
             acc_batch_score += batch_score / log_every
 
-            acc_sigma_min_score += self._compute_score_at_t(x=x, t=self.sigma_min, label=label) / log_every
-            acc_sigma_mean_score += self._compute_score_at_t(x=x, t=(self.sigma_min + self.sigma_max) / 2, label=label) / log_every
-            acc_sigma_max_score += self._compute_score_at_t(x=x, t=self.sigma_max, label=label) / log_every
+            # eval
+            sigma_min_loss, sigma_min_score = self._eval_at_t(x=x, t=self.sigma_min, label=label)
+            sigma_mean_loss, sigma_mean_score = self._eval_at_t(x=x, t=(self.sigma_min + self.sigma_max), label=label)
+            sigma_max_loss, sigma_max_score = self._eval_at_t(x=x, t=self.sigma_max, label=label)
+            acc_sigma_min_loss += sigma_min_loss / log_every
+            acc_sigma_min_score += sigma_min_score / log_every
+            acc_sigma_mean_loss += sigma_mean_loss / log_every
+            acc_sigma_mean_score += sigma_mean_score / log_every
+            acc_sigma_max_loss += sigma_max_loss / log_every
+            acc_sigma_max_score += sigma_max_score / log_every
             if verbose and (i == 0 or (i + 1) % log_every == 0):
                 pbar.set_postfix(
                     loss=acc_batch_loss,
                     score=acc_batch_score,
+                    sigma_min_loss=acc_sigma_min_loss,
+                    sigma_mean_loss=acc_sigma_mean_loss,
+                    sigma_max_loss=acc_sigma_max_loss,
                     sigma_min_score=acc_sigma_min_score,
                     sigma_mean_score=acc_sigma_mean_score,
                     sigma_max_score=acc_sigma_max_score
                 )
                 acc_batch_loss, acc_batch_score = 0., 0.
+                acc_sigma_min_loss, acc_sigma_mean_loss, acc_sigma_max_loss = 0., 0., 0.
                 acc_sigma_min_score, acc_sigma_mean_score, acc_sigma_max_score = 0., 0., 0.
             if i == n_iters:
                 break
@@ -80,10 +94,10 @@ class PFGMPPGuided(PFGMPP):
     def _train_classifier_step(self, *, x: Tensor, label: LongTensor):
         self.cls.train()
         x, label = x.to(self.device), label.to(self.device)
-        t = self.sample_from_sigma_prior(x.shape[0]).to(self.device)
+        t = self.cls_sample_from_sigma_prior(x.shape[0]).to(self.device)
         x_hat = self.sample_from_posterior(x=x, t=t)
         logits = self.cls(x=x_hat, t=t)
-        loss = F.cross_entropy(logits, label).mean()
+        loss = (F.cross_entropy(logits, label)).mean()
 
         self.cls_optimizer.zero_grad()
         loss.backward()
@@ -93,15 +107,19 @@ class PFGMPPGuided(PFGMPP):
         batch_score = torch.eq(torch.argmax(logits, dim=1), label).to(torch.float32).mean().item()
         return batch_loss, batch_score
 
-    def _compute_score_at_t(self, x: Tensor, t: float, label: LongTensor): 
+    @torch.no_grad()
+    def _eval_at_t(self, x: Tensor, t: float, label: LongTensor):
+        self.cls.eval()
         t = torch.full((len(x),), t).to(x.device)
         x_hat = self.sample_from_posterior(x=x, t=t)
         logits = self.cls(x=x_hat, t=t)
-        return torch.eq(torch.argmax(logits, dim=1), label).to(torch.float32).mean().item()
+        loss = F.cross_entropy(logits, label).mean().item()
+        score = torch.eq(torch.argmax(logits, dim=1), label).to(torch.float32).mean().item()
+        return loss, score
 
     def save(self, save_dir: str):
         os.makedirs(save_dir, exist_ok=True)
-        torch.save(self.model.state_dict(), os.path.join(save_dir, "cls.pt"))
+        torch.save(self.cls.state_dict(), os.path.join(save_dir, "cls.pt"))
         super().save(save_dir=save_dir)
 
     def load(self, load_dir: str):
