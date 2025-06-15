d3cdbc49c67028a52f5af583d9e3d6c16dbad8f0
d-shlenskii <daniil.shlenskii@gmail.com>
2025-06-15 17:47:18 +0300

    ibmd: training_loop added (teacher_net processing is needed)

--- src/ibmd/training/training_loop.py
+++ src/ibmd/training/training_loop.py
@@ -1,11 +1,63 @@
 from typing import Callable, Optional
 
+import torch.nn as nn
+from hydra.utils import instantiate
 from tqdm import tqdm
 
 from ibmd.core.ibmd import IBMD
 
 
+def training_loop(
+    *,
+    batch_size: int,
+    inner_problem_iters: int,
+    n_iters: int,
+    #
+    teacher_net: nn.Module,
+    teacher_dynamics_config: dict,
+    teacher_loss_fn_config: dict,
+    student_net_optimizer_config: dict,
+    student_data_estimator_net_optimizer_config: dict,
+    teacher_loss_dynamics_key: str = "pfgmpp",
+    n_classes: int = None,
+    ema_decay: float = 0.999,
+    #
+    log_every: int = 500,
+    eval_every: int = 500,
+    callback: Optional[Callable] = None,
+    save_path: Optional[str] = None,
+    verbose: bool = True,
+    #
+    device: str = "cpu",
+):
+    teacher_dynamics = instantiate(teacher_dynamics_config)
+    # teacher_net = instantiate(teacher_net_config).to(device)
+    teacher_loss_fn = instantiate(teacher_loss_fn_config, **{teacher_loss_dynamics_key: teacher_dynamics})
+    ibmd = IBMD(
+        teacher_dynamics=teacher_dynamics,
+        teacher_net=teacher_net,
+        teacher_loss_fn=teacher_loss_fn,
+        student_net_optimizer_config=student_net_optimizer_config,
+        student_data_estimator_net_config=student_data_estimator_net_optimizer_config,
+        n_classes=n_classes,
+        ema_decay=ema_decay,
+    )
+    training_loop_instantiated(
+        ibmd=ibmd,
+        #
+        batch_size=batch_size,
+        inner_problem_iters=inner_problem_iters,
+        n_iters=n_iters,
+        #
+        log_every=log_every,
+        eval_every=eval_every,
+        callback=callback,
+        save_path=save_path,
+        verbose=verbose,
+    )
+
 def training_loop_instantiated(
+    *,
     ibmd: IBMD,
     #
     batch_size: int,
