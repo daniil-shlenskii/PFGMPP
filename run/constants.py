dbab85ed61bb8f00016a7d6a530cfe7ff47f4d74
d-shlenskii <daniil.shlenskii@gmail.com>
2025-06-27 12:23:49 +0300

    notebook paths restructured

--- run/constants.py
+++ run/constants.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+# Project Structure
 PROJECT_DIR = str(Path(__file__).resolve().parent.parent)
 ARTIFACTS_DIR = os.path.join(PROJECT_DIR, "artifacts")
 
+# Paths to EDM utils for loading EDM-like models
 EDM_UTILS_DIR = os.path.join(PROJECT_DIR, "src", "edm_utils")
 TORCH_UTILS_DIR = os.path.join(EDM_UTILS_DIR, "torch_utils")
 DNNLIB_DIR = os.path.join(EDM_UTILS_DIR, "dnnlib")
